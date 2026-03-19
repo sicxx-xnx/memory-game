1b48e2a3a17aa8cfc4128dcc8de99536a3e747fa
Peyton <hpeyton33@gmail.com>
2026-03-19 18:44:36 -0500

    add util folder

---
+++ src/utili/gametype.ts
@@ -0,0 +1,46 @@
+
+const gameTypeOption: any = [
+    {food:[
+        "cheeseburger",
+        "Pizza",
+        "french fries",
+        'smothies',
+        'chicken wings',
+        'steak',
+        'porkchop',
+        'tacos',
+        'shrimp',
+        'ramen'
+    ]},
+    {pokemon:[
+        "Pikachu",
+        "Charizard",
+        "Bulbasaur",
+        "Squirtle",
+        "Eevee",
+        "Mewtwo",
+        "Gengar",
+        "Snorlax",
+        "Lucario",
+        "Jigglypuff"
+]},
+    {tvShows:[
+        "Breaking Bad",
+        "Stranger Things",
+        "Game of Thrones",
+        "The Office",
+        "Friends",
+        "The Mandalorian",
+        "The Witcher",
+        "The Boys",
+        "Succession",
+        "The Last of Us",
+        "Better Call Saul",
+        "Sherlock",
+        "The Crown",
+        "Rick and Morty",
+        "House of the Dragon"
+]}
+]
+
+export {gameTypeOption}
