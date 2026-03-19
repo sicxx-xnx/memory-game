9c1a6df4c3add0041a884011fe4ad4c876db97ac
Peyton <hpeyton33@gmail.com>
2026-03-19 07:03:39 -0500

    add type for api response and type to build new object built from api response. built function that converts apiresponse to said object

--- src/components/holder.tsx
+++ src/components/holder.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { Header } from "./header";
 import { apifetchstring } from "../utili/api";
-
+import type { imageobj } from "../types/image";
 
 export function Holder(){
 const [score, setScore] = useState<Number>(0) 
 const[gametype,setGameType] = useState<String>("Food") 
+const [images, setImages] = useState<imageobj[]>([])
 useEffect(()=>{
 if (gametype === "Food") {
  const apiCall =  async () => {
---
+++ src/types/apiresponsegiphy.ts
@@ -0,0 +1,11 @@
+export interface apiresponsegiphy {
+data: {
+    images: {
+        original: {
+            url: string
+        }
+    },
+    id: number | String,
+    title: string
+} 
+}
